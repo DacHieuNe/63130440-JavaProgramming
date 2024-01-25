c8687f08cfdae759958993bd296d0a341c5bd6f4
DacHieu <checkerpssss@gmail.com>
2024-01-25 12:09:38 +0700

    Finish lab 2

---
+++ lab--2/src/Exercise3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Exercise3 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhap vao so dien su dung cua thang: ");
+		double soDien = scanner.nextDouble();
+		if(soDien >= 0 && soDien <= 50) {
+			System.out.printf("Tien dien cua thang: %.2f",soDien*1000);
+		}
+		if(soDien > 50) {
+			System.out.printf("Tien dien cua thang: %.2f",soDien*1200);
+		}
+	}
+}
